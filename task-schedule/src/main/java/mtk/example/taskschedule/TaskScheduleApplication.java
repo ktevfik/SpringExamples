4c8e7c1264cececbd5798585b69661ee680cae5e
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-09-26 22:17:28 +0300

    Task Scheduling

---
+++ task-schedule/src/main/java/mtk/example/taskschedule/TaskScheduleApplication.java
@@ -0,0 +1,15 @@
+package mtk.example.taskschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class TaskScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskScheduleApplication.class, args);
+    }
+
+}
---
+++ task-schedule/src/test/java/mtk/example/taskschedule/TaskScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package mtk.example.taskschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
