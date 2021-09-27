33d04f8e934ed78889c53e5f4ff4936310afe261
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-09-27 15:51:39 +0300

    Authenticating with LDAP

---
+++ authenticating-ldap/src/main/java/mtk/example/authenticatingldap/HomeController.java
@@ -0,0 +1,14 @@
+package mtk.example.authenticatingldap;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Welcome to the home page!";
+    }
+}
