4e6fc97aa6c0cf2f18c5ed71f97bf25fe437c2cb
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-09-27 18:31:41 +0300

    Messaging with RabbitMQ

---
+++ messaging-rabbitmq/src/test/java/mtk/example/messagingrabbitmq/MessagingRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package mtk.example.messagingrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
